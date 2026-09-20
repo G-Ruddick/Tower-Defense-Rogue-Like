3852a7c4ed5a7da4787bffa15a2717dd014e2d48
Orangesuit1 <glen@ruddicks.net>
2026-09-20 02:03:09 -0700

    Added Tower icon selling and clickablility

--- tower-defense-roguelike/Scripts/Game Scene/PlayerManager.cs
+++ tower-defense-roguelike/Scripts/Game Scene/PlayerManager.cs
@@ -1,21 +1,33 @@
 using Godot;
+using System.Collections.Generic;
 using System;
 
 public partial class PlayerManager : Node {
 	[ExportCategory("Player Stats")]
 	[Export] private int money;
 	[Export] private int lives; 
-	[Export] private int numberOfTowers; 
+	[Export] private int maxNumberOfTowers; 
 
-	public Node3D[] towers;
+	public List<string> towers = new List<string>();
+	public List<Node2D> abilities = new List<Node2D>();
+	[Export] public Node playerTowersNode;
+
+	// singleton
+	public static PlayerManager instance {
+		get;
+		private set;
+	}
 
 	public override void _Ready() {
+		instance = this;
+
 		// temporary value assignment
 		money = 100;
 		lives = 20;
-		numberOfTowers = 1;
+		maxNumberOfTowers = 5;
 
-		towers = new Node3D[numberOfTowers];
+		towers.Add("Archer Tower");
+		UpdateTowers();
 	}
 	
 	// Money Functions
@@ -38,5 +50,47 @@ public partial class PlayerManager : Node {
 	}
 	public void ChangeLives(int value) {
 		lives += value;
+	}
+
+	// adding towers to the players inventory
+	public void UpdateTowers() {
+		float x = 102f, x1 = 380f, x2 = 788f;
+		Vector2 position = new Vector2(0, 96);
+
+		// resetting towers
+		foreach (TowerIcon tower in playerTowersNode.GetChildren()) {
+			tower.QueueFree();
+		}
+
+		for(int i = 0; i < towers.Count; i++) {
+			// creating icon
+			PackedScene icon = GD.Load<PackedScene>("res://Prefabs/Tower Icons/" + towers[i] + " Icon.tscn");
+			TowerIcon newIcon = (TowerIcon)icon.Instantiate();
+			newIcon.Name = towers[i];
+			playerTowersNode.AddChild(newIcon);
+
+			// positioning icon;
+			if (towers.Count % 2 == 1) {
+				if (i == (float)towers.Count / 2) {
+					position.X = (x2 + x1) / 2;
+				}
+				else if (i < (float)towers.Count / 2) {
+					position.X = ((x2 + x1) / 2) - (x * i);
+				}
+				else if (i > (float)towers.Count / 2) {
+					position.X = (x2 + x1) / 2 + (x * (towers.Count - i));
+				}
+			}
+			else {
+				if (i <= towers.Count / 2) {
+					position.X = (x2 + x1) / 2 - (x * (towers.Count / 2 - i)) + (x / 2);
+				}
+				else if (i > towers.Count / 2) {
+					position.X = x1 + (x * i) + (x / 2);
+				}
+			}
+
+			newIcon.Position = position;
+		}
 	}
 }
--- tower-defense-roguelike/Scripts/Game Scene/Towers/TowerIcon.cs
+++ tower-defense-roguelike/Scripts/Game Scene/Towers/TowerIcon.cs
@@ -1,5 +1,96 @@
 using Godot;
 
-public partial class TowerIcon : Node {
-	public Sprite2D icon;
+public partial class TowerIcon : Node2D {
+	[ExportCategory("Variables")]
+	[Export] public bool enabled;
+	[Export] public bool clickable;
+	[Export] public bool placeable;
+	[Export] private string towerName;
+	
+	[ExportCategory("Components")]
+	[Export] private Sprite2D icon;
+	[Export] private Area2D button;
+	[Export] private CollisionShape2D clickableArea;
+	[Export] public ButtonTemplate SellButton;
+	[Export] private Label Cost;
+
+	public override void _Ready() {
+		if (icon == null) {
+			GD.PrintErr(this.Name + "'s Sprite2D has no assigned sprite.");
+			GetTree().Quit();
+		}
+		if (clickableArea == null) {
+			GD.PrintErr(this.Name + "'s CollisionShape2D has no assigned shape.");
+			GetTree().Quit();
+		}
+
+		enabled = true;
+		clickable = false;
+		placeable = true;
+		SellButton.Visible = false;
+
+		button.MouseEntered += () => clickable = true;
+		button.MouseExited += () => clickable = false;
+	}
+
+	public override void _Input(InputEvent @event) {
+		if (@event.IsActionPressed("Select")) {
+			if (SellButton.GetClickability()) {
+				SellTower();
+			}
+
+			if (GetClickability()) {
+				OnClick();
+			}
+		}
+	}
+
+	public override void _ExitTree() {
+		PlayerManager.instance.CallDeferred(nameof(PlayerManager.instance.UpdateTowers));
+	}
+
+	public string GetTowerName() {
+		return towerName;
+	}
+
+	// Clickability for if the button can be touched
+	public bool GetClickability() {
+		return (enabled & clickable);
+	}
+	public void ChangeClickability() {
+		clickable = !clickable;
+	}
+	
+	// determines if the player is to be placing a tower or not
+	public bool GetPlaceable() {
+		return placeable;
+	}
+	public void ChangePlaceable() {
+		placeable = !placeable;
+	}
+
+	// Disabled the button entirely
+	public void DisableIcon() {
+		icon.Modulate = enabled ? new Color(20, 20, 20, 1) : new Color(1, 1, 1, 1);
+		button.Visible = !button.Visible;
+		enabled = !enabled;
+	}
+
+	public void SellTower() {
+		PlayerManager.instance.ChangeMoney(25);
+		PlayerManager.instance.towers.Remove(GetTowerName());
+		this.QueueFree();
+	}
+
+	public void OnClick() {
+		SellButton.Visible = !SellButton.Visible;
+		icon.Modulate = SellButton.Visible ? new Color(1.35f, 1.35f, 1.35f, 1) : new Color(1, 1, 1, 1);
+
+		// if (placeable) {
+			
+		// }
+		// else {
+
+		// }
+	}
 }
