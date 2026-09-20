1e7b714fab5c6d87048e52497800fc855fc24ee5
G <glen@ruddicks.net>
2026-09-19 20:34:29 -0700

    Added Archer Tower and tower types

--- tower-defense-roguelike/Scripts/Game Scene/PlayerManager.cs
+++ tower-defense-roguelike/Scripts/Game Scene/PlayerManager.cs
@@ -5,11 +5,17 @@ public partial class PlayerManager : Node {
 	[ExportCategory("Player Stats")]
 	[Export] private int money;
 	[Export] private int lives; 
+	[Export] private int numberOfTowers; 
+
+	public Node3D[] towers;
 
 	public override void _Ready() {
 		// temporary value assignment
 		money = 100;
 		lives = 20;
+		numberOfTowers = 1;
+
+		towers = new Node3D[numberOfTowers];
 	}
 	
 	// Money Functions
--- tower-defense-roguelike/Scripts/Game Scene/Towers/TowerClass.cs
+++ tower-defense-roguelike/Scripts/Game Scene/Towers/TowerClass.cs
@@ -2,29 +2,14 @@ using Godot;
 using System;
 
 public partial class TowerClass : Node {
-	// name of tower 
-	protected string towerName;
-	public string GetTowerName() {
-		return towerName;
-	}
-	
-	// cost to buy and sell tower
-	protected int towerCost;
-	public int GetCost() {
-		return towerCost;
-	}
-	
-	// determines if tower can be placed on water, land, or both
-	protected bool? aquatic;
-	public bool? GetAquatic() {
-		return aquatic;
-	}
+	[ExportCategory("Variables")]
+	[Export] public string towerName;
+	[Export] public int towerCost;
+	[Export] public bool aquatic; // determines if tower can be placed on water, land, or both
 
-	// determines if tower is floating or not 
-	protected bool floating;
-	public bool GetFloating() {
-		return floating;
-	}
+	[ExportCategory("Components")]
+	[Export] private Sprite3D towerSprite;
+	[Export] public Area3D towerRadius;
 
 	// Destroying Tower
 	public void SellTower(bool x) {
@@ -38,12 +23,4 @@ public partial class TowerClass : Node {
 		
 		this.QueueFree();
 	}
-
-	[Export] protected AnimatedSprite2D towerSprite;
-	
-	protected float towerRange;
-	[Export] protected Area2D towerRangeRadius;
-
-	protected int[] towerSize;
-	[Export] protected Area2D towerRadius;
 }
